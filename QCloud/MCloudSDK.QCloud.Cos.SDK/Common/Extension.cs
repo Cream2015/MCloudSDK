a3b870bf17df7d1bfb2e60186dc9b886cfc2a8c0
Cream2015 <343224963@qq.com>
2017-11-02 16:35:21 +0800

    init

---
+++ QCloud/MCloudSDK.QCloud.Cos.SDK/Common/Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCloudSDK.QCloud.Cos.SDK.Common
+{
+    public static class Extension
+    {
+        public static long ToUnixTime(this DateTime nowTime)
+        {
+            //DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1, 0, 0, 0, 0));
+            DateTime startTime = new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime();
+            return (long)Math.Round((nowTime - startTime).TotalMilliseconds, MidpointRounding.AwayFromZero);
+        }
+    }
+}
